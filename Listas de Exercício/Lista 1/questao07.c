175d4aebb7afbbdae0fe4ff9fc0a3036021c8a9b
Yuri Thomaz <ytcarvalho14@gmail.com>
2024-05-25 23:13:27 -0300

    Add files via upload

---
+++ Listas de Exercício/Lista 1/questao07.c
@@ -0,0 +1,27 @@
+/*
+Lista de Exercícios I - Questão #07
+Yuri Thomaz dos Santos Carvalho - 202410520
+*/
+
+/*
+Escreva um programa que leia um número inteiro e, utilizando o operador %, converta ele num valor entre 1 e 6.
+Imprima o resultado na tela;
+*/
+
+#include <stdio.h>
+
+int main() {
+
+  int numero = 0, conversor = 0;
+
+  printf("\nDigite um número inteiro: ");
+  scanf("%d", &numero);
+
+  ( numero < 0 ) ? numero = (-numero): numero;
+  ( numero > 6 || numero < 1 ) ? conversor = (numero % 6) : (conversor = numero);
+
+  printf("O resultado é %d\n", conversor);
+
+  return 0;
+
+}
