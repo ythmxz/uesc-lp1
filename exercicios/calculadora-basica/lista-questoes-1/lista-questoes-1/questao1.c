cbf09c0a6154b4860fb9bfd9724490695e305a48
ythmxz <ytcarvalho14@gmail.com>
2024-08-06 16:09:11 -0300

    Add question 1.c

---
+++ exercicios/calculadora-basica/lista-questoes-1/lista-questoes-1/questao1.c
@@ -0,0 +1,43 @@
+/*
+Yuri Thomaz dos Santos Carvalho - 202410520
+Lista de Questões 1 - Questão 1
+*/
+
+/*
+Defina, descreva e explique:
+
+a) Linguagem de Programação;
+b) Contexto histórico e as várias vertentes de programação;
+c) Níveis de Linguagens de Programação;
+d) Compilador;
+e) Algoritmos e suas características;
+*/
+
+#include <stdio.h>
+
+int questao1() {
+
+    printf("\na) Uma linguagem de programação é um conjunto de regras sintáticas, "
+           "semânticas e comandos que permitem ao programador especificar as ações "
+           "que o computador deverá realizar.\n");
+
+    printf("\nb) As linguagens de Programação se diversificaram bastante ao longo do tempo e "
+           "seguiram por diversos caminhos diferentes, podendo ser classificadas por seu paradigma, "
+           "sua estrutura de tipos, sua plataforma de desenvolvimento, entre outros fatores.\n");
+
+    printf("\nc) As linguagens de Programação são categorizadas em níveis de acordo "
+           "com a proximidade de comunicação direta com o hardwares. Eles são 'baixo nível', "
+           "que engloba linguagem de máquina e Assembly. 'alto nível', que engloba a maior parte "
+           "das linguagens, como C, Python, Java, etc...\n");
+
+    printf("\nd) Um compilador é um programa que lê um código feito em uma linguagem de alto "
+           "nível e traduz os comandos para a linguagem de máquina, o que permite o computador "
+           "realizá-los e rodar o código ou programa.\n");
+
+    printf("\ne) Algorítmos são um conjunto de passos que são utilizados para realizar "
+           "alguma ação ou resolver algum problema. Normalmente eles são sequenciais, organizados "
+           "e sistematizados e não existem apenas no mundo digital.\n");
+
+    return 0;
+
+}
