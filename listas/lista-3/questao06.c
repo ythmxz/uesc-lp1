264afcaad40196e50eaae3a179a7f8339bd17c67
Yuri <ytcarvalho14@gmail.com>
2025-03-20 16:05:53 -0300

    Add question 6

--- listas/lista-3/questao06.c
+++ listas/lista-3/questao06.c
@@ -17,6 +17,37 @@ Para arrays 3 dimensionais, desenvolva:
 
 int main() {
 
+	int tamanhoX = 10;
+	int tamanhoY = 20;
+	int tamanhoZ = 5;
+
+	int valor = 0;
+
+	int array[tamanhoX][tamanhoY][tamanhoZ];
+
+	printf("\n");
+
+	for ( int i = 0; i < tamanhoX; i++ ) {
+
+		for ( int j = 0; j < tamanhoY; j++ ) {
+
+			for (int k = 0; k < tamanhoZ; k++ ) {
+
+				array[i][j][k] = valor;
+				valor++;
+
+				printf("[%03d] ", array[i][j][k]);
+
+			}
+
+			printf("\n");
+
+		}
+
+		printf("\n");
+
+	}
+
 	return 0;
 
 }
